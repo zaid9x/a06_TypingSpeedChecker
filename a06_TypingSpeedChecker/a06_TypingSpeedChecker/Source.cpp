69cf21cd3460931d8e394fda198c60e49b8beddf
Zaid Baig <ZaidBaig@cmail.carleton.ca>
2019-06-15 17:15:11 -0400

    New Change: My Actual Program

--- a06_TypingSpeedChecker/a06_TypingSpeedChecker/Source.cpp
+++ a06_TypingSpeedChecker/a06_TypingSpeedChecker/Source.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+using namespace std;
 
 
 void main()
 {
-	std::cout << "Hello World!" << std::endl;
+	int rows, count = 0, count1 = 0, k = 0;
 
+	cout << "Welcome to Pyramid Maker!\n\n";
+	cout << "Enter number of rows: ";
+	cin >> rows;
+
+	cout << "\n\n";
+	for (int i = 1; i <= rows; ++i)
+	{
+		for (int space = 1; space <= rows - i; ++space)
+		{
+			cout << "  ";
+			++count;
+		}
+
+		while (k != 2 * i - 1)
+		{
+			if (count <= rows - 1)
+			{
+				cout << i + k << " ";
+				++count;
+			}
+			else
+			{
+				++count1;
+				cout << i + k - 2 * count1 << " ";
+			}
+			++k;
+		}
+		count1 = count = k = 0;
+
+		cout << endl;
+	}
+	
 }
